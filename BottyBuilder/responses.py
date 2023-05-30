bc9d2292c9bd21c19587f8affb8bb753cf63f281
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-05-29 19:27:49 -0700

    Update responses.py

--- BottyBuilder/responses.py
+++ BottyBuilder/responses.py
@@ -8,11 +8,26 @@ def get_response(message: str) -> str:
         return 'Hello! My name is BottyBuilder. I will help you achive your fitness goals!'
 
     if message == '!help':
-        return 'Here is the list of commands.\nTo find out your maintenance calorie level, type "!calories".\nFor exercises you can do at the gym, type "!gym".\nFor exercises you can do at home, type "!home".\nTo see a list of foods to facilitate bulking, type "!bulk".\nTo see a list of foods to facilitate cutting, type "!cut".\nFor encouragement, type "!motivation".'
+        return 'Here is the list of commands.\nTo find out your maintenance calorie level, type "!calories".\nFor exercises for each muscle group, type "!exercises".\nTo see a list of foods to facilitate bulking, type "!bulk".\nTo see a list of foods to facilitate cutting, type "!cut".\nFor encouragement, type "!motivation".'
 
     # Handle user input for calculating calories
-    if message == '!calories':
+    if message = '!calories':
         return 'Please enter your height in cm.'
+    
+    if message = '!exercises':
+        return 'Here is a list of muscle groups to train. The command \n
+    
+ 
+        
+    
+    if message = '!bulk':
+        return 'Here are a list of foods that are high in calorie.\n'
+    
+    if message = '!cut':
+        return 'Here are a list of foods that are low in calorie.\n"
+    
+    if message = '!motivation':
+        
     
     # figure out how to get bot to retrieve info through discord
 
